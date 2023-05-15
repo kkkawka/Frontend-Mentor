c3eae20d0158283da4b3151ad1cba4fb82407a9e
kkkawka <kacperkone@gmail.com>
2023-05-15 18:27:47 +0200

    commit

--- interactive-rating-component-main/js.js
+++ interactive-rating-component-main/js.js
@@ -1,16 +1,27 @@
 const hideElement = document.querySelector('.before-submit')
 const showElement = document.querySelector('.after-submit')
 const submit = document.querySelector('.submit-btn')
-const pkt = document.querySelector('.shape')
+const getPoints = document.querySelectorAll('.shape')
 const points = document.querySelector('#rated-points')
 
+submit.disabled = true
 
-submit.addEventListener('click', change);
-function change() {
-hideElement.classList.add('display-none')
-showElement.classList.remove('display-none')};
+// idk
+getPoints.forEach((get)=> {
+  get.addEventListener('click', ()=>{
+    let taken = get.value
+    points.innerHTML = taken
+    submit.disabled = false //temporary
+    submit.classList.remove('.disabled-remove') //temporary
+    submit.style.backgroundColor = 'rgb(251, 116, 19)'// temporary
+  })
+})
 
-pkt.addEventListener('click', addpkt)
-function addpkt(){
-    points.innerHTML = 'x'
-}
+// switch "main" element, by adding or removing class "display-none"
+submit.addEventListener('click', () => {
+  hideElement.classList.add('display-none')
+  showElement.classList.remove('display-none')
+ 
+  
+})
+
